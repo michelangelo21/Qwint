55692516acf2fb75d1505447a106a726527294fa
Michał Łukomski <michal.lukomski21@gmail.com>
2022-05-29 15:48:27 +0200

    target

--- gui.py
+++ gui.py
@@ -30,110 +30,158 @@ class App(tk.Tk):
         num_wires = 6
 
         # for player 1
-        topframe = tk.Frame(self)
-        # topframe.pack(side=tk.TOP, fill='both', expand=True)
-        topframe.grid(row=0, column=0, sticky='nsew')
-
-        midframe = tk.Frame(self)
-        midframe.grid(row=1, column=0, sticky='nsew')
+        self.topframe = tk.Frame(self)
+        self.topframe.grid(row=0, column=0, sticky='nsew')
 
         # for player 2 
-        bottomframe = tk.Frame(self)
-        # bottomframe.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
-        bottomframe.grid(row=2, column=0, sticky='nsew')
+        self.bottomframe = tk.Frame(self)
+        self.bottomframe.grid(row=2, column=0, sticky='nsew')
 
-
-
-        # for plot
-        leftframe = tk.Frame(midframe)
-        leftframe.pack(side='left')
+        self.plotframe = tk.Frame(self)
+        self.plotframe.grid(row=1, column=0, sticky='nsew')
 
         # for choosing wires
-        rightframe = tk.Frame(midframe)
-        rightframe.pack(side='right', fill='both', expand=True)
+        self.control2_frame = tk.Frame(self)
+        self.control2_frame.grid(row=1, column=1, sticky='nsew')
+
+        self.control1_frame = tk.Frame(self)
+        self.control1_frame.grid(row=1, column=2, sticky='nsew')
+
+
+        self.target_frame = tk.Frame(self)
+        self.target_frame.grid(row=1, column=3, sticky='nsew')
+
+        self.control2_lbl = tk.Label(self.control2_frame, text='Control\nqubit')
+        self.control2_wire = tk.IntVar()
+        self.radio_control2 = [
+            tk.Radiobutton(self.control2_frame, text="q"+str(i), variable=self.control2_wire, value=i, command=self.show_apply)
+            for i in range(num_wires)
+        ]
+
+        self.control1_lbl = tk.Label(self.control1_frame, text='Control\nqubit')
+        self.control1_wire = tk.IntVar()
+        self.radio_control1 = [
+            tk.Radiobutton(self.control1_frame, text="q"+str(i), variable=self.control1_wire, value=i, command=self.show_apply)
+            for i in range(num_wires)
+        ]
+        
+        self.target_lbl = tk.Label(self.target_frame, text='Target\nqubit')
+        self.target_wire = tk.IntVar()
+        self.radio_target = [
+            tk.Radiobutton(self.target_frame, text="q"+str(i), variable=self.target_wire, value=i, command=self.show_apply)
+            for i in range(num_wires)
+        ]
+
+
+        self.rightframe = tk.Frame(self)
+        self.rightframe.grid(row=1, column=4, sticky='nsew')
 
         all_gates = [
             'H', 'X'
         ]
 
         self.qc = qiskit.QuantumCircuit(num_wires)
+        self.initial_circuit()
+        self.replot()
+
         self.round_no = 1
         self.active_player = 1
 
-        self.p1_deck = []
-        self.p2_deck = []
+        self.p1_deck = [*(["H"]*12), *(["X"]*12),*(["CX"]*6)]
+        self.p2_deck = self.p1_deck.copy()
 
         self.p1_hand = []
+        self.draw(10, self.p1_deck, self.p1_hand)
         self.p2_hand = []
+        self.draw(10, self.p2_deck, self.p2_hand)
 
         self.p1_pass = False
         self.p2_pass = False
 
-        
 
 
 
-        qc = qiskit.QuantumCircuit(num_wires,num_wires)
-        qc.h(range(num_wires))
-        qc.cnot(0,1)
-        qc.h(2)
-        qc.x(2)
-        for i in range(10):
-            qc.h(2)
-
-        state = Statevector.from_instruction(qc)
-        fig_bloch = plot_bloch_multivector_vertical(state)   
-
-        fig_qc = qc.draw("mpl")
-
-        fig_qc.set_size_inches(10, 6)
-        fig_qc.tight_layout()
-
-        fig_bloch.set_size_inches(2, 6)
-        fig_bloch.tight_layout()
-
-        # create FigureCanvasTkAgg object
-        fig_qc_canvas = FigureCanvasTkAgg(fig_qc, leftframe)
-        fig_qc_canvas.get_tk_widget().pack(side=tk.LEFT, expand=0)
-
-        fig_bloch_canvas = FigureCanvasTkAgg(fig_bloch, leftframe)
-        fig_bloch_canvas.get_tk_widget().pack(side=tk.LEFT, expand=0)
-
-        wire_choice = tk.IntVar()
-        self.radio_wire = [
-            tk.Radiobutton(leftframe, text="q"+str(i), variable=wire_choice, value=i)#.pack(side=tk.TOP, fill=tk.X, expand=1)
-            for i in range(num_wires)
-        ]
 
         p1_hand = random.choices(all_gates, k=10)
         p1_hand.sort()
         p2_hand = random.choices(all_gates, k=10)
         p2_hand.sort()
 
+        self.apply_button = tk.Button(self.rightframe, text="Apply", command=self.show_apply)
+
         self.p1_choice = tk.IntVar()
         self.p2_choice = tk.IntVar()
 
-        for i in range(len(p1_hand)):
-            tk.Radiobutton(topframe, text=p1_hand[i], variable=self.p1_choice, value=i, command= lambda: self.show_wire_choices(p1_hand[i])).pack(side=tk.LEFT, fill=tk.X, expand=1)
 
-        for i in range(len(p2_hand)):
-            tk.Radiobutton(bottomframe, text=p2_hand[i], variable=self.p2_choice, value=i).pack(side=tk.LEFT, fill=tk.X, expand=1)
+        self.show_p1_choice()
+        # self.show_p2_choice()
 
-        tk.Button(rightframe, text='Quit', command=self.quit).pack()
+
+        tk.Button(self.rightframe, text='Quit', command=self.quit).pack()
+        tk.Button(self.rightframe, text='replot', command=self.replot).pack()
+
+    # end __init__
+
+    def replot(self):
+        try:
+            self.fig_qc_canvas.get_tk_widget().destroy()
+            self.fig_bloch_canvas.get_tk_widget().destroy()
+        except:
+            pass
+
+        state = Statevector.from_instruction(self.qc)
+        fig_bloch = plot_bloch_multivector_vertical(state)   
+
+        fig_qc = self.qc.draw("mpl")
+
+        fig_qc.set_size_inches(10, 6)
+        fig_qc.tight_layout()
+
+        fig_bloch.set_size_inches(2,6)
+        fig_bloch.tight_layout()
+
+        # create FigureCanvasTkAgg object
+        self.fig_qc_canvas = FigureCanvasTkAgg(fig_qc, self.plotframe)
+        self.fig_qc_canvas.get_tk_widget().pack(side=tk.LEFT, expand=0)
+
+        self.fig_bloch_canvas = FigureCanvasTkAgg(fig_bloch, self.plotframe)
+        self.fig_bloch_canvas.get_tk_widget().pack(side=tk.LEFT, expand=0)
 
     def draw(self, n_cards, deck, hand):
         for i in range (n_cards):
-            n_gate = random.randint(0, len(deck)-1)
-            hand.append(deck[n_gate])
-            del deck[n_gate]
+            gate = random.choice(deck)
+            hand.append(gate)
+            deck.remove(gate)
+        hand.sort()
 
+    def show_apply(self):
+        self.apply_button.pack()
 
-    def show_wire_choices(self, gate):
-        for wire in self.radio_wire:
+    def show_radio_control2(self):
+        self.control2_lbl.pack(side=tk.TOP, fill=tk.X)
+        for wire in self.radio_control2:
             wire.pack(side=tk.TOP, fill=tk.X, expand=1)
 
+    def show_radio_control1(self):
+        self.control1_lbl.pack(side=tk.TOP, fill=tk.X)
+        for wire in self.radio_control1:
+            wire.pack(side=tk.TOP, fill=tk.X, expand=1)
+
+    def show_radio_target(self):
+        self.target_lbl.pack(side=tk.TOP, fill=tk.X)
+        for wire in self.radio_target:
+            wire.pack(side=tk.TOP, fill=tk.X, expand=1)
+
+    def show_wire_choices(self, gate):
+        self.show_radio_target()
+
     def show_p1_choice(self):
-        pass
+        for i in range(len(self.p1_hand)):
+            tk.Radiobutton(self.topframe, text=self.p1_hand[i], variable=self.p1_choice, value=i, command= lambda: self.show_wire_choices(self.p1_hand[i])).pack(side=tk.LEFT, fill=tk.X, expand=1)
+
+    def show_p2_choice(self):
+        for i in range(len(self.p2_hand)):
+            tk.Radiobutton(self.bottomframe, text=self.p2_hand[i], variable=self.p2_choice, value=i).pack(side=tk.LEFT, fill=tk.X, expand=1)
 
     def initial_circuit(self):
         pass
