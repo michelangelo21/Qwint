aabe97bc19c92dc1cbc97fe7dd4c423dd84686ec
Michał Łukomski <michal.lukomski21@gmail.com>
2022-05-29 13:47:38 +0200

    hand in gui

--- qwint.py
+++ qwint.py
@@ -11,9 +11,20 @@ qc.h([0,1,2,3,4])
 qc.draw('mpl')
 plt.show()
 
-gates = [
+all_gates = [
     'H', 'X'
 ]
+
+deck = [*(["H"]*10), *(["X"]*10)]
+deck.remove("H")
+gates = random.choices(deck, k=10)
+deck.remove(gates)
+
+hand_1 = []
+gate = random.choice(all_gates)
+hand_1.append(gate)
+hand_1[0] = 'h'
+
 
 qiskit.circuit.library.XGate
 random.choices(gates, k=3)
